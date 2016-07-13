8b6c5e0cb6dcb6b1febcf341b5ac3052af801493
Aleksandar Savic <alek_bn@hotmail.com>
2016-07-13 14:35:43 +0200

    Spajanje grane "filip" na granu "filip+aleksandar"...
    2) Acton2 je sada stanje koje predstavlja klasu ConditionList.
    Klasa MyPanel je zamijenila klasu JPanel.

--- src/gui/model/states/Action2.java
+++ src/gui/model/states/Action2.java
@@ -1,5 +1,8 @@
 package gui.model.states;
 
+import gui.model.ConditionList;
+import gui.model.MyPanel;
+
 import java.awt.BorderLayout;
 import java.awt.Cursor;
 import java.awt.Rectangle;
@@ -14,111 +17,37 @@ import javax.swing.JPanel;
 
 public class Action2 implements State {
 
-	/*
-	 * 	  String	listData[] =
-			{
-				"Item 1",
-				"Item 2",
-				"Item 3",
-				"Item 4"
-			};
+	private MyPanel myPanel;
 
-			// Create a new listbox control
-			JList<String> listbox = new JList<String>( listData );
-			JPanel kljPanel = new JPanel();
-			kljPanel.add( listbox, BorderLayout.CENTER );
-		    
-		//	  frame.getContentPane().add(kljPanel, BorderLayout.SOUTH);
-
-	 */
-	private	JPanel myPanel;
-	private static final int NONE = -1;
-	private static final int BORDER = 3;
-	private int startX = NONE;
-	private int startY = NONE;
-	private int prevX = NONE;
-	private int prevY = NONE;
-	private boolean resize = false;
-	
-	public Action2(JPanel panel) {
+	public Action2(MyPanel panel) {
 		this.myPanel = panel;
 	}
-	
+
 	@Override
 	public void drawComponent(int X, int Y, char panel) {
 		// TODO Auto-generated method stub
 		if (panel == 'a') {
-			JButton button = null;
-		
-			System.out.println("ACTION2");
-			button = new JButton("ACTION2");
-			button.setBounds(X, Y, 80, 24);
-	
-			button.addMouseListener(new MouseAdapter() {
-		      public void mouseReleased(MouseEvent e) {
-		        startX = NONE;
-		        startY = NONE;
-		        ((JComponent) e.getSource()).setCursor(Cursor.getDefaultCursor());
-		      }
-	
-		      public void mousePressed(MouseEvent e) {
-		        startX = e.getX();
-		        startY = e.getY();
-		      }
-		    });
-		    
-		    /* S T A C K   O V E R   F L O W  LOW */
-			button.addMouseMotionListener(new MouseMotionAdapter() {
-		      public void mouseMoved(MouseEvent e) {
-		        JComponent source = (JComponent) e.getSource();  // komponenta nad kojom radimo
-		        int x = e.getX();
-		        int y = e.getY();
-		        Rectangle bounds = source.getBounds();  // granice komponente nad kojom radimo
-		        // risajz -=- ako je x ili y u minimalnoj granici ( BORDER-u ) -=- OR-ovi nam govore sa koje strane pomijeramo
-		        resize = x < BORDER || y < BORDER || Math.abs(bounds.width - x) < BORDER || Math.abs(bounds.height - y) < BORDER;
-		        if (resize) {
-		          // TODO: there are a lot of resize cursors here, this is just of proof of concept
-		          source.setCursor(Cursor.getPredefinedCursor(Cursor.E_RESIZE_CURSOR));
-		        } else { // pomijeramo
-		          source.setCursor(Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR));
-		        }
-		      }
-	
-		      public void mouseDragged(MouseEvent e) {
-		        int x = e.getX();
-		        int y = e.getY();
-		        if (startX != NONE && startY != NONE) { // znaci da je PRITISNUT klik misa.
-		        										// kad se klik PUSTI, start-ovi su NONE.
-		          JComponent source = (JComponent) e.getSource();  // komponenta nad kojom radimo
-		          Rectangle bounds = source.getBounds();  // granice komponente nad kojom radimo
-		          int deltaX = x - startX;
-		          int deltaY = y - startY;
-		          if (resize) {
-		            // TODO: handle all resize cases, left, right,...
-		            source.setSize(Math.max(10, bounds.width + x - prevX), Math.max(10, bounds.height + y - prevY));
-		          } else { // pomijeramo
-		            source.setLocation(bounds.x + deltaX, bounds.y + deltaY);
-		          }
-		          // TODO: make sure you don't resize it as much as it disappears
-		          // TODO: make sure you don't move it outside the main panel
-		        } else { // znaci da JE PUSTEN (NIJE PRITISNUT) klik misa i da su start-ovi NONE
-		          startX = x;
-		          startY = y;
-		        }
-		        prevX = x;
-		        prevY = y;
-		      }
-		    });
+			ConditionList myWidg = ConditionList.getInstance();
+
+			if (myWidg == null) {
+				System.out.println("NULL - VEC DODATA - POMJERI");
+			}
+
+			System.out.println("ACTION1");
+			myWidg.setBounds(X, Y, 180, 240);
+
+			myPanel.add(myWidg);
+			myPanel.widgList.add(myWidg);
 			
-		    myPanel.add(button);
-		    myPanel.repaint();
-		}
-		else System.out.println("\nWrong panel bro!\n");
-	
+			myPanel.revalidate();
+			myPanel.repaint();
+		} else
+			System.out.println("\nWrong panel bro!\n");
+
 	} // kraj metode drawComponent
 
 	@Override
 	public String toString() {
-		return "Action2 State";
+		return "Action1 State";
 	}
 }
